62398f99397b72fb18572222f1633bd9369b7497
Thaiquocse <Thaiquocse@gmail.com>
2024-05-08 20:18:52 +0700

    Add HeloController

---
+++ Base.API/Controllers/HeloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Base.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Helo()
+        {
+            var helo =  "helo";
+            return Ok(helo);               
+        }
+    }
+}
