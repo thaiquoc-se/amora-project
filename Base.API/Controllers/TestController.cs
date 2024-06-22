f6b9f77e00e43ee5eafef4c1be04311532b3a326
Thaiquocse <Thaiquocse@gmail.com>
2024-06-23 00:10:43 +0700

    Test Again

---
+++ Base.API/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Base.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet] public ActionResult Index()
+        {
+            return Ok("CI/CD successfully");
+        }
+    }
+}
